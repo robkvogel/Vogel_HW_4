dc011afe6480b91e2805868a2768b8d898650154
Rob Vogel <rob.vogel@kellogg.northwestern.edu>
2024-03-10 16:44:22 +0000

    more controller stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,26 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome Back!"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Nope. Invalid"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope. Invalid"
+      redirect_to "/login"
+    end
 
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "So Long, Partner"
+    redirect_to "/login"
   end
 end
   
