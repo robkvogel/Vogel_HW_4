803c3305a1de051ee99b263a7ac4869c949357de
Rob Vogel <rob.vogel@kellogg.northwestern.edu>
2024-03-10 19:16:01 +0000

    next

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
 
-  def set_current_user
+  def current_user
     puts "------------------ code before every request ------------------"
     @current_user = User.find_by({ "id" => session["user_id"] })
     @user = User.all
   end
-
-  def current_user
-    @current_user
-  end
-
 
 end
